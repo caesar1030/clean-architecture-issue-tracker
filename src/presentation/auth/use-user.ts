ddde96b1ab39eb3ab6ea1296877b0afcde238bd4
zlx454545 <zlx454545@gmail.com>
2024-01-07 13:26:53 +0900

    feat: use user 커스텀 훅 구현

---
+++ src/presentation/auth/use-user.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { container } from '../../di/inversify.config';
+import { TYPES } from '../../di/types';
+import { GetUserUseCase } from '../../domain/use-case/auth/get-user';
+
+export default function useUser() {
+  const getUserUseCase = container.get<GetUserUseCase>(TYPES.GetUserUseCase);
+
+  const {
+    isLoading,
+    data: user,
+    error,
+  } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => getUserUseCase.invoke(),
+  });
+
+  return {
+    isLoading,
+    user,
+    error,
+    isAuthenticated: user?.role === 'authenticated',
+  };
+}
