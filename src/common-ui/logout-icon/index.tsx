c8ecf50ace8a0600fa7a771c389003ec975a0700
zlx454545 <zlx454545@gmail.com>
2024-01-15 14:28:06 +0900

    design: 공통 logout icon 컴포넌트 css 수정

--- src/common-ui/logout-icon/index.tsx
+++ src/common-ui/logout-icon/index.tsx
@@ -3,6 +3,6 @@ import { ComponentPropsWithoutRef } from 'react';
 interface LogoutIconProps extends ComponentPropsWithoutRef<'img'> {}
 
 function LogoutIcon(_: LogoutIconProps) {
-  return <img src="/public/logout.svg" alt="logout" />;
+  return <img src="/public/logout.svg" className="w-8 h-8" alt="logout" />;
 }
 export default LogoutIcon;
