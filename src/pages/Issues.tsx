025b8d24a7ccf028ca97163739b59be31e78f631
zlx454545 <zlx454545@gmail.com>
2024-01-15 15:44:54 +0900

    feat: label milestone link 구현

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -3,17 +3,35 @@ import Button from '../common-ui/button';
 import IssueFilterBar from '../presentation/issue/issue-filter-bar';
 import IssueFilterResetButton from '../presentation/issue/issue-filter-reset-button';
 import IssueTable from '../presentation/issue/issue-table';
+import TabButton from '../common-ui/tab-button';
+import useLabels from '../presentation/label/use-labels';
+import useMilestones from '../presentation/milestone/use-milestones';
 
 function Issues() {
+  const { labels } = useLabels();
+  const { milestones } = useMilestones();
   return (
     <>
       <div className="flex justify-between">
         <IssueFilterBar />
-        <Link to={'/new-issue'}>
-          <Button size="S" variant="contained">
-            이슈 작성
-          </Button>
-        </Link>
+        <div className="flex gap-4">
+          <TabButton>
+            <Button size="M" variant="ghosts">
+              <img src="/public/label-bold.svg" alt="라벨" />
+              <span>라벨 ({labels?.data.length})</span>
+            </Button>
+            <Button size="M" variant="ghosts">
+              <img src="/public/milestone-bold.svg" alt="마일스톤" />
+              <span>마일스톤({milestones?.data.length})</span>
+            </Button>
+          </TabButton>
+
+          <Link to={'/new-issue'}>
+            <Button size="S" variant="contained">
+              이슈 작성
+            </Button>
+          </Link>
+        </div>
       </div>
 
       <IssueFilterResetButton />
