36cb64a4301430ba2088510856a3aacd374ed0eb
zlx454545 <zlx454545@gmail.com>
2024-01-24 20:10:25 +0900

    feat: 이슈 열기 닫기 기능 구현

--- src/presentation/issue/issue-detail/issue-detail-header/index.tsx
+++ src/presentation/issue/issue-detail/issue-detail-header/index.tsx
@@ -3,6 +3,8 @@ import IssueTitle from './issue-title';
 import Button from '../../../../common-ui/button';
 import EditIssueTitleForm from './edit-issue-title-form';
 import { IssueResponse } from '../../../../domain/model/issue/response';
+import useCloseIssues from '../../use-close-issues';
+import useOpenIssues from '../../use-open-issues';
 
 interface IssueDetailHeaderProps {
   issue: IssueResponse['data'] | undefined;
@@ -10,6 +12,8 @@ interface IssueDetailHeaderProps {
 
 function IssueDetailHeader({ issue }: IssueDetailHeaderProps) {
   const [isEditingTitle, setIsEditingTitle] = useState(false);
+  const { closeIssues } = useCloseIssues();
+  const { openIssues } = useOpenIssues();
 
   const toggleIsEditingTitle = () => {
     setIsEditingTitle((prev) => !prev);
@@ -32,8 +36,16 @@ function IssueDetailHeader({ issue }: IssueDetailHeaderProps) {
         <Button size="S" variant="outline" onClick={toggleIsEditingTitle}>
           제목 편집
         </Button>
-        <Button size="S" variant="outline">
-          이슈 닫기
+        <Button
+          size="S"
+          variant="outline"
+          onClick={() =>
+            issue?.isOpen
+              ? closeIssues({ issueIds: [issue!.id] })
+              : openIssues({ issueIds: [issue!.id] })
+          }
+        >
+          {issue?.isOpen ? '이슈 닫기' : '이슈 열기'}
         </Button>
       </div>
     </div>
