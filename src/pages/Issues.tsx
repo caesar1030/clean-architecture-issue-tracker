9227e9999fbb489511778a99194e32dc2a1e48c1
zlx454545 <zlx454545@gmail.com>
2024-03-21 02:13:07 +0900

    fix: issues 페이지 navbar 수정

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -1,39 +1,20 @@
-import { Link } from 'react-router-dom';
 import Button from '../common-ui/button';
 import IssueFilterBar from '../presentation/issue/issue-filter-bar';
 import IssueFilterResetButton from '../presentation/issue/issue-filter-reset-button';
 import IssueTable from '../presentation/issue/issue-table';
-import TabButton from '../common-ui/tab-button';
-import useLabels from '../presentation/label/use-labels';
-import useMilestones from '../presentation/milestone/use-milestones';
-import labelBoldIcon from '../assets/label-bold.svg';
-import milestoneBoldIcon from '../assets/milestone-bold.svg';
+import Navbar from '../common-ui/navbar';
 
 function Issues() {
-  // TODO: 훅 제거
-  const { labels } = useLabels();
-  const { milestones } = useMilestones();
   return (
     <>
       <div className="flex justify-between">
         <IssueFilterBar />
         <div className="flex gap-4">
-          <TabButton>
-            <Button size="M" variant="ghosts">
-              <img src={labelBoldIcon} alt="라벨" />
-              <span>라벨 ({labels.length})</span>
-            </Button>
-            <Button size="M" variant="ghosts">
-              <img src={milestoneBoldIcon} alt="마일스톤" />
-              <span>마일스톤({milestones?.data.length})</span>
-            </Button>
-          </TabButton>
+          <Navbar />
 
-          <Link to={'/new-issue'}>
-            <Button size="S" variant="contained">
-              이슈 작성
-            </Button>
-          </Link>
+          <Button to="/new-issue" size="S" variant="contained">
+            이슈 작성
+          </Button>
         </div>
       </div>
 
