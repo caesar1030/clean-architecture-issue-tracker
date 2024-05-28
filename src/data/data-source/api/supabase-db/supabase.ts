e69fcb938403204e62e8974872c7460e449780cb
zlx454545 <zlx454545@gmail.com>
2024-05-28 17:35:14 +0900

    chore: vite migration

--- src/data/data-source/api/supabase-db/supabase.ts
+++ src/data/data-source/api/supabase-db/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_API_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
