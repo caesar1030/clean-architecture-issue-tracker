f80fca0a8e0474b33de6def1d393a7c7683deed2
zlx454545 <zlx454545@gmail.com>
2024-06-06 02:45:10 +0900

    fix: user schema 수정

--- src/schemas/issue/issue-schema.ts
+++ src/schemas/issue/issue-schema.ts
@@ -6,4 +6,7 @@ export const IssueSchema = z.object({
     .min(1, '제목을 입력해주세요.')
     .max(30, '제목은 최대 30글자입니다.'),
   contents: z.string().max(500, '내용은 최대 500글자입니다.').optional(),
+  label: z.any().optional(),
+  milestone: z.any().optional(),
+  assignee: z.any().optional(),
 });
