e0569af96ea7c238e8de69b1fe2370779231f98d
zlx454545 <zlx454545@gmail.com>
2024-06-06 13:21:20 +0900

    feat: comment edit validation 구현

--- src/presentation/comment/comment-contents/edit-comment-contents-form/index.tsx
+++ src/presentation/comment/comment-contents/edit-comment-contents-form/index.tsx
@@ -1,72 +1,78 @@
-import { ComponentPropsWithRef, ForwardedRef, forwardRef } from 'react';
-import grip from '@/assets/grip.svg';
-import Table from '@/common-ui/table';
-import Avatar from '@/common-ui/avatar';
-import { getTimeDiff } from '@/utils/helpers';
-import InformationTag from '@/common-ui/information-tag';
 import { IssueResponse } from '@/model/issue/response';
+import useEditComment from '@/presentation/comment/use-edit-comment';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import Button from '@/common-ui/button';
+import EditCommentTextarea from '@/presentation/comment/comment-contents/edit-comment-contents-form/edit-comment-textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CommentSchema } from '@/schemas/comment/comment-schemat';
+import ErrorMessage from '@/common-ui/error-message';
 
-export interface EditIssueContentsFormProps
-  extends ComponentPropsWithRef<'textarea'> {
+export interface EditIssueContentsFormProps {
   comment: NonNullable<IssueResponse['data']['comments']>[number];
   issueAuthor: IssueResponse['data']['author'];
+  stopEditing: () => void;
+}
+interface FormType {
+  contents: string;
 }
 
-const EditCommentContentsForm = forwardRef(
-  (
-    { comment, issueAuthor, ...rest }: EditIssueContentsFormProps,
-    ref: ForwardedRef<HTMLTextAreaElement>
-  ) => {
-    return (
-      <>
-        <div className="border rounded-large border-neutral-border-active">
-          <Table columns="1fr" size="L">
-            <Table.Header>
-              <div className="flex justify-between">
-                <div className="flex gap-2 items-center">
-                  <Avatar src={comment.author.avatar} />
-                  <span className="text-M text-neutral-text-strong">
-                    {comment.author.nickname}
-                  </span>
-                  <span className="text-M text-neutral-text-weak">
-                    {getTimeDiff(comment.createdAt)} 전
-                  </span>
-                </div>
-
-                <div className="flex gap-4 items-center">
-                  {comment.author.id === issueAuthor.id && (
-                    <InformationTag variant="writer">작성자</InformationTag>
-                  )}
-                </div>
-              </div>
-            </Table.Header>
-            <Table.Row>
-              <textarea
-                {...rest}
-                ref={ref}
-                value={rest.value}
-                className="w-full bg-inherit focus:outline-none text-neutral-text-strong text-M grow resize-none"
-              />
-
-              <div className="flex gap-2 justify-end">
-                <span className="text-neutral-text-weak text-S">{`띄어쓰기 포함 ${
-                  (rest.value as string)?.length
-                }자`}</span>
-
-                <img
-                  width={20}
-                  height={20}
-                  src={grip}
-                  alt="그립"
-                  className="w-5 h-5"
-                />
-              </div>
-            </Table.Row>
-          </Table>
-        </div>
-      </>
+const EditCommentContentsForm = ({
+  comment,
+  issueAuthor,
+  stopEditing,
+}: EditIssueContentsFormProps) => {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<FormType>({
+    defaultValues: {
+      contents: comment.contents || '',
+    },
+    resolver: zodResolver(CommentSchema),
+    mode: 'onChange',
+  });
+  const { editComment } = useEditComment();
+  const onSubmit: SubmitHandler<FormType> = ({ contents }) => {
+    editComment(
+      { authorId: issueAuthor!.id, commentId: comment.id, contents },
+      { onSuccess: () => stopEditing() }
     );
-  }
-);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+      <Controller
+        name="contents"
+        control={control}
+        render={({ field }) => (
+          <EditCommentTextarea
+            issueAuthor={issueAuthor}
+            comment={comment}
+            {...field}
+          />
+        )}
+      />
+      <ErrorMessage>{errors.contents?.message}</ErrorMessage>
+
+      <div className="flex gap-2 justify-end">
+        <Button
+          size="S"
+          variant="outline"
+          onClick={() => {
+            stopEditing();
+            reset();
+          }}
+          type="button"
+        >
+          <span>편집 취소</span>
+        </Button>
+        <Button size="S" variant="contained" type="submit" disabled={!isValid}>
+          <span>편집 완료</span>
+        </Button>
+      </div>
+    </form>
+  );
+};
 
 export default EditCommentContentsForm;
--- src/presentation/comment/comment-contents/index.tsx
+++ src/presentation/comment/comment-contents/index.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import useUser from '@/presentation/auth/use-user';
 import Button from '@/common-ui/button';
 import Table from '@/common-ui/table';
 import Avatar from '@/common-ui/avatar';
 import { getTimeDiff } from '@/utils/helpers';
 import InformationTag from '@/common-ui/information-tag';
-import useEditComment from '@/presentation/comment/use-edit-comment';
 import EditCommentContentsForm from '@/presentation/comment/comment-contents/edit-comment-contents-form';
 import { IssueResponse } from '@/model/issue/response';
 import editIcon from '@/assets/edit.svg';
@@ -18,56 +16,17 @@ interface CommentContentsProps {
 
 const CommentContents = ({ issueAuthor, comment }: CommentContentsProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const { control, handleSubmit, reset } = useForm<FormType>();
-  const { editComment } = useEditComment();
   const { user } = useUser();
 
-  interface FormType {
-    contents: string;
-  }
-
-  const onSubmit: SubmitHandler<FormType> = ({ contents }) => {
-    editComment(
-      { authorId: user!.id, commentId: comment.id, contents },
-      { onSuccess: () => setIsEditing(false) }
-    );
-  };
+  const stopEditing = () => setIsEditing(false);
 
   if (isEditing)
     return (
-      <>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
-          <Controller
-            name="contents"
-            control={control}
-            defaultValue={comment.contents || ''}
-            render={({ field }) => (
-              <EditCommentContentsForm
-                issueAuthor={issueAuthor}
-                comment={comment}
-                {...field}
-              />
-            )}
-          />
-
-          <div className="flex gap-2 justify-end">
-            <Button
-              size="S"
-              variant="outline"
-              onClick={() => {
-                setIsEditing(false);
-                reset();
-              }}
-              type="button"
-            >
-              <span>편집 취소</span>
-            </Button>
-            <Button size="S" variant="contained" type="submit">
-              <span>편집 완료</span>
-            </Button>
-          </div>
-        </form>
-      </>
+      <EditCommentContentsForm
+        comment={comment}
+        issueAuthor={user!}
+        stopEditing={stopEditing}
+      />
     );
 
   return (
@@ -108,4 +67,5 @@ const CommentContents = ({ issueAuthor, comment }: CommentContentsProps) => {
     </>
   );
 };
+
 export default CommentContents;
