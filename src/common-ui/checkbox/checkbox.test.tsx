1cbcfc9180123ec878b99bd2c2bf4999fe652ee6
zlx454545 <zlx454545@gmail.com>
2024-06-12 22:32:43 +0900

    test: checkbox 테스트코드 추가

--- src/common-ui/checkbox/checkbox.test.tsx
+++ src/common-ui/checkbox/checkbox.test.tsx
@@ -3,12 +3,20 @@ import { render } from '@/tests/test-utils/render-with-context';
 import userEvent from '@testing-library/user-event';
 
 describe('Checkbox 컴포넌트', () => {
-  it('checked가 true로 주어질 경우 check된 상태여야 한다.', () => {
+  it('checked가 true로 주어질 경우 check 된 상태여야 한다.', () => {
     const { getByRole } = render(
       <Checkbox id="id" label="label" checked={true}></Checkbox>
     );
 
     expect(getByRole('checkbox')).toBeChecked();
+  });
+
+  it('checked가 false로 주어질 경우 check 되지 않은 상태여야 한다.', () => {
+    const { getByRole } = render(
+      <Checkbox id="id" label="label" checked={false}></Checkbox>
+    );
+
+    expect(getByRole('checkbox')).not.toBeChecked();
   });
 
   it('클릭할 경우, 주어진 onChange가 호출되어야 한다.', async () => {
